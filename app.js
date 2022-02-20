8127af9962167acc09c242fad91aaecf709890b3
Forever Young <dy9040@gmail.com>
2022-02-20 12:01:24 -0600

    added more HTML elements to code.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer');
-// const fs = require ('fs');
-// const generatePage = require ('./src/page-template.js')
+const fs = require ('fs');
+const generatePage = require ('./src/page-template.js')
 
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
 //   if (err) throw new Error(err); 
 
-//   console.log('Portfolio complete! Check out index.html to see the output!')
-// });
+// console.log('Portfolio complete! Check out index.html to see the output!')
+// // });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -145,6 +145,12 @@ const promptUser = () => {
 
   promptUser()
   .then(promptProject)
-  .then (portfolioData => {
-    console.log(portfolioData);
-  })
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+  });
